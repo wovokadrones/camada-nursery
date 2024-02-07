ee437e59b67545343aec20f57f524f642e5ec4aa
geoscriber <100984391+geoscriber@users.noreply.github.com>
2024-02-07 22:42:42 +0800

    commit

---
+++ data.js
@@ -0,0 +1,389 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-top-view",
+      "name": "Top View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 0.28504088417225404,
+        "pitch": 1.3188086938025805,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0219503334439484,
+          "pitch": 1.092161922154549,
+          "rotation": 0,
+          "target": "1-nursery-1"
+        },
+        {
+          "yaw": 1.0739473898440615,
+          "pitch": 0.811872587884281,
+          "rotation": 0,
+          "target": "4-nursery-4"
+        },
+        {
+          "yaw": -2.2329376240990335,
+          "pitch": 1.4781182287537078,
+          "rotation": 0,
+          "target": "3-nursery-3"
+        },
+        {
+          "yaw": -0.06330895323661956,
+          "pitch": 1.0259750378395687,
+          "rotation": 0,
+          "target": "2-nursery-2"
+        },
+        {
+          "yaw": -0.0453067869727839,
+          "pitch": 0.7674204450314654,
+          "rotation": 0,
+          "target": "5-nursery-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-nursery-1",
+      "name": "Nursery 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 2.750528705166473,
+        "pitch": 0.48393075513032,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.004485511712243,
+          "pitch": -0.20195977068250848,
+          "rotation": 0,
+          "target": "4-nursery-4"
+        },
+        {
+          "yaw": 0.4761764779582158,
+          "pitch": 0.013594284093112918,
+          "rotation": 0,
+          "target": "2-nursery-2"
+        },
+        {
+          "yaw": -0.2481511017891478,
+          "pitch": 0.06175401072732711,
+          "rotation": 0,
+          "target": "3-nursery-3"
+        },
+        {
+          "yaw": 1.5171493331944355,
+          "pitch": 0.031884856589773136,
+          "rotation": 0,
+          "target": "5-nursery-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-nursery-2",
+      "name": "Nursery 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3061412582641143,
+          "pitch": 0.033348175615543596,
+          "rotation": 0,
+          "target": "3-nursery-3"
+        },
+        {
+          "yaw": -0.6212218195630435,
+          "pitch": -0.017094994793250606,
+          "rotation": 0,
+          "target": "1-nursery-1"
+        },
+        {
+          "yaw": -1.018913845292765,
+          "pitch": -0.13422164110103196,
+          "rotation": 0,
+          "target": "4-nursery-4"
+        },
+        {
+          "yaw": -2.1708455248723286,
+          "pitch": -1.2669609228477938,
+          "rotation": 0,
+          "target": "0-top-view"
+        },
+        {
+          "yaw": -2.767506438843606,
+          "pitch": 0.040276961100937925,
+          "rotation": 0,
+          "target": "5-nursery-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-nursery-3",
+      "name": "Nursery 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6236039376421445,
+          "pitch": 0.05792609864811915,
+          "rotation": 0,
+          "target": "1-nursery-1"
+        },
+        {
+          "yaw": -0.6385112263971457,
+          "pitch": -0.17777403044539497,
+          "rotation": 0,
+          "target": "4-nursery-4"
+        },
+        {
+          "yaw": -1.884275692791082,
+          "pitch": 0.09525185195644781,
+          "rotation": 0,
+          "target": "2-nursery-2"
+        },
+        {
+          "yaw": -2.0676483689494063,
+          "pitch": 0.00672424227866486,
+          "rotation": 0,
+          "target": "5-nursery-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-nursery-4",
+      "name": "Nursery 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.401002522803216,
+          "pitch": 0.27883341306118403,
+          "rotation": 0,
+          "target": "1-nursery-1"
+        },
+        {
+          "yaw": 0.09670221383244737,
+          "pitch": 0.11402985605656113,
+          "rotation": 0,
+          "target": "2-nursery-2"
+        },
+        {
+          "yaw": -0.6991995833637201,
+          "pitch": 0.08428927401637942,
+          "rotation": 0,
+          "target": "3-nursery-3"
+        },
+        {
+          "yaw": 0.6729931376164053,
+          "pitch": -0.6331405013087483,
+          "rotation": 0,
+          "target": "0-top-view"
+        },
+        {
+          "yaw": 0.7879864480317895,
+          "pitch": 0.03291774840730888,
+          "rotation": 0,
+          "target": "5-nursery-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-nursery-5",
+      "name": "Nursery 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8651794607259928,
+          "pitch": 0.20278750374177257,
+          "rotation": 0,
+          "target": "2-nursery-2"
+        },
+        {
+          "yaw": 1.1650055855122226,
+          "pitch": -0.02365255446907355,
+          "rotation": 0,
+          "target": "3-nursery-3"
+        },
+        {
+          "yaw": 0.2306495672083937,
+          "pitch": -0.058805931186636684,
+          "rotation": 0,
+          "target": "1-nursery-1"
+        },
+        {
+          "yaw": -0.26418513455437065,
+          "pitch": -0.06720397411297796,
+          "rotation": 0,
+          "target": "5-nursery-5"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "CAMADA Nursery",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
